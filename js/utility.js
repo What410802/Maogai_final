0c0b64b741d3c73d5a1a632b303cb431ac3c093f
what410802 <what410802@126.com>
2025-12-01 01:25:03 +0800

    Another some non-breaking changes.

--- js/utility.js
+++ js/utility.js
@@ -1,12 +1,13 @@
+const impressAPI = impress();
+impressAPI.init();
+
 document.addEventListener(`DOMContentLoaded`, () => {
-	const impressAPI = impress();
-	impressAPI.init();
 	const contentItems = document.getElementById(`content-page`).querySelectorAll(`ol li`);
 	document.addEventListener(`impress:stepenter`, (e) => {
 		const currStep = e.target; // Both src and target are the page TO COME, i.e. the page "current".
 		if(currStep.classList.contains(`ret-step`)){
 			contentItems.forEach(it => it.classList.remove(`toc-active`));
-			const targetItem = document.getElementById(currStep.getAttribute(`mygo`));
+			const targetItem = document.getElementById(currStep.getAttribute(`mygo2`));
 			if(targetItem){
 				targetItem.classList.add(`toc-active`); // Unused: , `shake`, `shake-constant`, `shake-slow`, `shake-little`
 			}
@@ -16,3 +17,10 @@ document.addEventListener(`DOMContentLoaded`, () => {
 		}
 	});
 });
+document.addEventListener(`keydown`, (event) => {
+	if(event.ctrlKey && (event.key === 'o' || event.key === 'Escape')){
+		console.log(`Ctrl+${event.key} pressed. Going to "myfinal".`);
+		event.preventDefault();
+		impressAPI.goto(`myfinal`);
+	}
+})
